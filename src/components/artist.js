ab9f62b09ce8133ad5ef90e91b82e151a4c129b5
shwetank007 <shwetank121339@gmail.com>
2018-05-16 23:33:38 +0530

    Feat: Data Show in the artist component

--- src/components/artist.js
+++ src/components/artist.js
@@ -3,11 +3,43 @@ import React,  { Component } from 'react';
 //component
 import Header from './header';
 
+const REQ_URL = `http://localhost:3004/artists`;
+
 class Artist extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            artist: ''
+        }
+    }
+
+    componentDidMount() {
+        fetch(`${REQ_URL}/${this.props.match.params.artistId}`, {
+            method: 'GET'
+        }).then(response => response.json())
+        .then(json => {
+            this.setState({
+                artist:json
+            });
+        })
+    }
+
     render() {
         return(
             <div>
                 <Header/>
+                <div className="artist-bio">
+                    <div className="avatar">
+                        <span style={{background:`url('/images/cover/${this.state.artist.cover}.jpg') no-repeat`}}></span>
+                    </div>
+                    <div className="bio">
+                        <h3>{this.state.artist.name}</h3>
+                        <div className="bio-text">
+                            {this.state.artist.bio}
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
